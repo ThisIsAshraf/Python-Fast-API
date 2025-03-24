894e123368a1825e8b969df4c46df3cdf3b7791c
Ashraful Alam Shihab <ash@azsystem.com>
2025-03-25 03:19:16 +0600

    Authentication setup successfull

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,9 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schema 
+from . import schema, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_Scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -34,7 +35,11 @@ def verify_access_token(token: str, credentials_exception):
         raise credentials_exception
     return token_data
 
-def get_current_user(token: str = Depends(oauth2_Scheme)):
+def get_current_user(token: str = Depends(oauth2_Scheme), db: Session = Depends(database.get_db)):
     credentails_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Colud not validate the credentials", headers={"WWW-Authenticate": "Bearer"})
     
-    return verify_access_token(token, credentails_exception)
+    token = verify_access_token(token, credentails_exception)
+    
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    
+    return user
--- app/routers/post.py
+++ app/routers/post.py
@@ -13,15 +13,15 @@ router = APIRouter(
 
 # Get all Post
 @router.get("/", response_model=List[schema.Post])
-def test_post(db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def test_post(db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
 # Create Post
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schema.Post)
-def create_post(post: schema.PostCreate, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def create_post(post: schema.PostCreate, db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
-    print(user_id)
+    print(current_user.email)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -30,7 +30,7 @@ def create_post(post: schema.PostCreate, db: Session = Depends(get_db), user_id 
 
 # Get Post by ID
 @router.get("/{id}", response_model=schema.Post)
-def get_post(id: int, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def get_post(id: int, db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with ID: {id} was not found")
@@ -40,7 +40,7 @@ def get_post(id: int, db: Session = Depends(get_db), user_id : int = Depends(oau
 # Delete Post
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     deleted_post = db.query(models.Post).filter(models.Post.id == id)
     if deleted_post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exists")
@@ -50,7 +50,7 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id : int = Depends(
 
 # Update Post
 @router.put("/{id}", response_model=schema.Post)
-def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db), user_id : int = Depends(oauth2.get_current_user)):
+def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
     updated_post = post_query.first()
     if updated_post == None:
