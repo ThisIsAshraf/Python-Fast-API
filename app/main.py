edf13fe4aa0909b37c83cfc34f5d9bab391f65d8
Ashraful Alam Shihab <ash@azsystem.com>
2025-03-24 07:59:45 +0600

    User Registration Successfull
    User Registration complted. Comments added on main.py files

--- app/main.py
+++ app/main.py
@@ -7,11 +7,10 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
-from . import models, schema
+from . import models, schema, utils
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
-
 
 app = FastAPI();
 
@@ -26,16 +25,18 @@ while True:
         print("Error: ", error)
         time.sleep(2)
 
+# Test Route
 @app.get("/")
 async def root():
     return {"message": "Bismillahir Rahmanir Rahmin"}
 
-
+# Get all Post
 @app.get("/posts", response_model=List[schema.Post])
 def test_post(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
+# Create Post
 @app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schema.Post)
 def create_post(post: schema.PostCreate, db: Session = Depends(get_db)):
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
@@ -45,6 +46,7 @@ def create_post(post: schema.PostCreate, db: Session = Depends(get_db)):
     db.refresh(new_post)
     return new_post
 
+# Get Post by ID
 @app.get("/posts/{id}", response_model=schema.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
@@ -53,6 +55,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return post
 
         
+# Delete Post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -63,6 +66,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+# Update Post
 @app.put("/posts/{id}", response_model=schema.Post)
 def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
@@ -72,3 +76,27 @@ def update_post(id: int, post: schema.PostCreate, db: Session = Depends(get_db))
     post_query.update(post.dict(),synchronize_session= False)
     db.commit()
     return post_query.first()
+
+# Create User
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schema.UserCreateResponse)
+def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    
+    # Password Hash
+    # hashed_password= pwd_context.hash(user.password)
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
+
+# Get User by ID
+@app.get('/users/{id}', response_model=schema.UserCreateResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} not found")
+    return user
